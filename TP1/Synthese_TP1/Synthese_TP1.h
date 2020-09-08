9f7ef9bfa1abad2540a868a0497e1b16a7296a9c
Niccolas_Calvet <nicolas.calvetgp@gmail.com>
2020-09-08 16:04:39 +0200

    Compute gravity force, air resistance force and wind for paratrooper

---
+++ TP1/Synthese_TP1/Synthese_TP1.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <iostream>
+#include <fstream>
+
+#include <glm/glm.hpp>
+
+void writeResults(std::ofstream& file, glm::vec3 acceleration, glm::vec3 speed, glm::vec3 position);
+
+void printResults(glm::vec3 acceleration, glm::vec3 speed, glm::vec3 position);
+
+class Foo {
+	public:
+		Foo() : m_a(5) {
+		}
+
+	private:
+		int m_a = 5;
+
+		glm::vec3 m_v{1, 0, 0};
+};
