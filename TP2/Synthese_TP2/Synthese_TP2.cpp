ca143e1d22cb8d2430649a4e524ecf443be0b815
Niccolas_Calvet <nicolas.calvetgp@gmail.com>
2020-09-15 15:15:36 +0200

    TP2 : setPixel tests

--- TP2/Synthese_TP2/Synthese_TP2.cpp
+++ TP2/Synthese_TP2/Synthese_TP2.cpp
@@ -19,8 +19,24 @@ int main()
     image.create(width, height);
 
 
-    
+    for (unsigned int x = 0; x < width; ++x) {
+        for (unsigned int y = 0; y < height; y++){
+            image.setPixel(x, y, sf::Color(
+                changeRange(x*width, (unsigned int)0, width * height, (unsigned int)0, (unsigned int)255),
+                changeRange(y*width, (unsigned int)0, width * height, (unsigned int)0, (unsigned int)255),
+                changeRange((unsigned int)0, (unsigned int)0, width * height, (unsigned int)0, (unsigned int)255)
+            ));
+        }
+    }
 
 
     image.saveToFile("../../../result.png");
+}
+
+template <typename T>
+T changeRange(T const& OldValue, T const& OldMin, T const& OldMax, T const& NewMin, T const& NewMax) {
+    T OldRange = (OldMax - OldMin);
+    T NewRange = (NewMax - NewMin);
+    T NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+    return NewValue;
 }
