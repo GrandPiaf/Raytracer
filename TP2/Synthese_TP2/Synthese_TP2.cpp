eedc886fd49915101237c350eae5fca876630cca
Niccolas_Calvet <nicolas.calvetgp@gmail.com>
2020-09-15 15:02:16 +0200

    TP2 : Writing image on disk

--- TP2/Synthese_TP2/Synthese_TP2.cpp
+++ TP2/Synthese_TP2/Synthese_TP2.cpp
@@ -14,32 +14,13 @@ int main()
     unsigned int width = 800;
     unsigned int height = 600;
 
-    sf::RenderWindow window(sf::VideoMode(width, height), "Raytracer");
-
     // Create image and fill it with calculated pixels
     sf::Image image;
-    image.create(width, height, sf::Color::Magenta); // Black
+    image.create(width, height);
 
-    // Create Sprite from Texture from Image
-    sf::Texture texture;
-    texture.loadFromImage(image);
 
-    // Sprite is the rendered image
-    sf::Sprite sprite;
-    sprite.setTexture(texture);
     
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
 
-        window.clear();
-        window.draw(sprite);
-        window.display();
-    }
+    image.saveToFile("../../../result.png");
 }
