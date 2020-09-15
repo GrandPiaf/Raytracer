c807ae350ed9dd11d759a6c5b1471716e5836302
Niccolas_Calvet <nicolas.calvetgp@gmail.com>
2020-09-15 16:11:55 +0200

    TP2 : quarter sphere

--- TP2/Synthese_TP2/Synthese_TP2.cpp
+++ TP2/Synthese_TP2/Synthese_TP2.cpp
@@ -1,9 +1,10 @@
-﻿// Synthese_TP2.cpp : définit le point d'entrée de l'application.
-//
+﻿
+#include <iostream>
 
 #include "Synthese_TP2.h"
-#include "glm/glm.hpp"
 
+#include "glm/glm.hpp"
+#include "glm/gtx/intersect.hpp"
 #include "SFML/Graphics.hpp"
 
 using namespace std;
@@ -14,18 +15,27 @@ int main()
     unsigned int width = 800;
     unsigned int height = 600;
 
-    // Create image and fill it with calculated pixels
+    // Create image
     sf::Image image;
     image.create(width, height);
 
+    // Sphere data
+    glm::vec3 center(0, 0, 10);
+    glm::float32 radius = 50;
 
+    // Ray direction
+    glm::vec3 direction(0, 0, 1); //normalized value !
+
+    // Compute pixel color
     for (unsigned int x = 0; x < width; ++x) {
         for (unsigned int y = 0; y < height; y++){
-            image.setPixel(x, y, sf::Color(
-                changeRange(x*width, (unsigned int)0, width * height, (unsigned int)0, (unsigned int)255),
-                changeRange(y*width, (unsigned int)0, width * height, (unsigned int)0, (unsigned int)255),
-                changeRange((unsigned int)0, (unsigned int)0, width * height, (unsigned int)0, (unsigned int)255)
-            ));
+            // Ray origin
+            glm::vec3 origin(x - width/2, y - height/2, 0);
+            glm::vec3 position;
+            glm::vec3 normal;
+            if (glm::intersectRaySphere(origin, direction, center, radius, position, normal)) {
+                image.setPixel(x, y, sf::Color::Green);
+            }
         }
     }
 
--- TP2/Synthese_TP2/Synthese_TP2.h
+++ TP2/Synthese_TP2/Synthese_TP2.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-#include <iostream>
+
 
 template <typename T>
 T changeRange(T const& OldValue, T const& OldMin, T const& OldMax, T const& NewMin, T const& NewMax);
